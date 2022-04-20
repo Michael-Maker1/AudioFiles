a55e2b373e4959ba35117c47f51e099869d8ea4d
michaelonwuka <michaelonwuka@localhost>
2022-04-20 10:02:57 +0200

    First Commit

---
+++ Media_Player_SommerSemester_VA06/src/AudioFile.java
@@ -0,0 +1,16 @@
+
+public class AudioFile {
+	// Frage: Was war noch als initiala Schritte notwendig in VA06, für das Projektsetup
+	//--> Tests runterladen, einfügen und als Quellcode markieren
+	//-->Junit-Bibliothek aktivieren
+	
+	// Frage: Wie wird so eine Java-Datei händisch zur Ausführung gebracht?
+	//--> javac AudioFile.java
+	//
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
